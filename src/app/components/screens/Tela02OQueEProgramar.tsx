abff77a02db26a4bb7310f9bbcebae60977a3335
rafaelski <rafaelski@gmail.com>
2026-04-24 16:17:17 -0300

    Update Tela02OQueEProgramar.tsx

--- src/app/components/screens/Tela02OQueEProgramar.tsx
+++ src/app/components/screens/Tela02OQueEProgramar.tsx
@@ -67,7 +67,7 @@ export function Tela02OQueEProgramar({ onPrevious, onNext }: Tela02OQueEPrograma
       onNext={onNext}
       layout={TELA02_LAYOUT}
       contentStageThemeClassName={TELA02_THEME.contentStageThemeClassName}
-      leftBottomClassName="max-w-[760px]"
+      leftBottomClassName="w-full"
       leftMain={
         <>
           <ScreenTitle title="O que é programar?" eyebrow="Começando a viagem" />
@@ -91,7 +91,6 @@ export function Tela02OQueEProgramar({ onPrevious, onNext }: Tela02OQueEPrograma
         <div className="relative animate-clipboard-sway [transform-origin:50%_14%]">
           <div className="relative">
             <div className="h-[120px] w-[400px] rounded-t-3xl border-[6px] border-black bg-[var(--sesc-orange)]"></div>
-            /* <div className="h-[40px] w-[400px] border-x-[6px] border-black bg-white"></div> */
             <div className="h-[120px] w-[400px] border-x-[6px] border-black bg-[var(--sesc-yellow-flower)]"></div>
             <div className="h-[40px] w-[400px] border-x-[6px] border-black bg-white"></div>
             <div className="h-[120px] w-[400px] rounded-b-3xl border-[6px] border-t-0 border-black bg-[var(--sesc-pink)]"></div>
