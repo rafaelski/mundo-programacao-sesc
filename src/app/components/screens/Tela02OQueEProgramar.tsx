88ee818c6d7e5a3eac400ebb9a4a07ad66bcc17b
rafaelski <rafaelski@gmail.com>
2026-04-24 16:11:02 -0300

    Update Tela02OQueEProgramar.tsx

--- src/app/components/screens/Tela02OQueEProgramar.tsx
+++ src/app/components/screens/Tela02OQueEProgramar.tsx
@@ -96,7 +96,7 @@ export function Tela02OQueEProgramar({ onPrevious, onNext }: Tela02OQueEPrograma
             <div className="h-[40px] w-[400px] border-x-[6px] border-black bg-white"></div>
             <div className="h-[120px] w-[400px] rounded-b-3xl border-[6px] border-t-0 border-black bg-[var(--sesc-pink)]"></div>
 
-            <div className="absolute -top-14 left-1/2 flex h-[60px] w-[380px] -translate-x-1/2 items-center justify-center rounded-t-full border-[6px] border-black bg-[var(--sesc-green-grass)] px-8 text-center">
+            <div className="absolute -top-4 left-1/2 flex h-[80px] w-[380px] -translate-x-1/2 items-center justify-center rounded-t-full border-[6px] border-black bg-[var(--sesc-green-grass)] px-8 text-center">
               <span
                 className={`text-[18px] font-black uppercase tracking-[0.16em] text-[var(--sesc-ink)] ${
                   isLineActive(typingStep, RECIPE_TITLE, 0, typingLines) ? 'recipe-writing-caret' : ''
